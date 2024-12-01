f5b4e14f054e1c2af3ad1eb550b1d138fdacbab6
PURVIK-31 <mepurviksharma@gmail.com>
2024-12-01 14:27:26 +0530

    removed a stuff

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <main className="bg-gray-100 min-h-screen">
+      <h1 className="bg-orange-400">hello there !</h1>
+    </main>
+  );
 };
 
 export default App;
