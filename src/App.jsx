2eb0f9d0fee9023412feefa20424b99004242257
PURVIK-31 <mepurviksharma@gmail.com>
2024-12-01 15:15:20 +0530

    finished the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
+import About from "./components/About";
+import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
+import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
-const App = () => {
+function App() {
   return (
-    <main className="bg-gray-100 min-h-screen">
-      <h1 className="bg-orange-400">hello there !</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   );
-};
+}
 
 export default App;
